225c24b2fad9285b9872fc1f8676132231222b19
hashirsajid58200p <hs58200p@gmail.com>
2026-08-08 19:17:59 +0500

    perf: remove unused chats_icon and razorpay_logo assets

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -8,13 +8,11 @@ import logo from './logo.svg'
 import dropdown_icon from './dropdown_icon.svg'
 import menu_icon from './menu_icon.svg'
 import cross_icon from './cross_icon.png'
-import chats_icon from './chats_icon.svg'
 import verified_icon from './verified_icon.svg'
 import arrow_icon from './arrow_icon.svg'
 import info_icon from './info_icon.svg'
 import upload_icon from './upload_icon.png'
 import stripe_logo from './stripe_logo.png'
-import razorpay_logo from './razorpay_logo.png'
 import Dermatologist from './Dermatologist.svg'
 import Gastroenterologist from './Gastroenterologist.svg'
 import General_physician from './General_physician.svg'
@@ -28,7 +26,6 @@ export const assets = {
     header_img,
     group_profiles,
     logo,
-    chats_icon,
     verified_icon,
     info_icon,
     profile_pic,
@@ -39,8 +36,7 @@ export const assets = {
     cross_icon,
     dropdown_icon,
     upload_icon,
-    stripe_logo,
-    razorpay_logo
+    stripe_logo
 }
 
 export const specialityData = [
