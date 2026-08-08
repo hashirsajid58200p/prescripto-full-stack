07856682e09ae35ce772b83b91b08cdb64d9a7e2
hashirsajid58200p <hs58200p@gmail.com>
2026-08-08 12:51:40 +0500

    fix: export default app in backend server.js

--- backend/server.js
+++ backend/server.js
@@ -26,4 +26,8 @@ app.get("/", (req, res) => {
   res.send("API Working")
 });
 
-app.listen(port, () => console.log(`Server started on PORT:${port}`))
+if (!process.env.VERCEL) {
+  app.listen(port, () => console.log(`Server started on PORT:${port}`))
+}
+
+export default app;
